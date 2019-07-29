b4ed6b180d0ab8c1d651295202ab170923ce24eb
NabienneBintAhmad <mamelena.ndiongue@uvs.edu.sn>
2019-07-29 08:56:41 +0000

    befor pull 2

--- src/Controller/WarriController.php
+++ src/Controller/WarriController.php
@@ -40,47 +40,5 @@ class WarriController extends AbstractController
             'controller_name'=>'WarriController',
         ]);
     }
-   
-        /**
-         * @Route("/register", name="register", methods={"POST", "GET"})
-         */
-        public function register(Request $request,UserPasswordEncoderInterface $passwordEncoder,EntityManagerInterface $entityManager,SerializerInterface $serializer,ValidatorInterface $validator)
-        {
-            $values = json_decode($request->getContent());
-            if(isset($values->email,$values->password)) 
-            {
-                $user = new User();
-
-                //$form=$this->createForm(UserTypee::class, $user);
-                //$form->handleRequest($request);
-                
-                $user->setEmail($values->email);
-                $user->setPassword($passwordEncoder->encodePassword($user,$values->password));
-                $user->setRoles($user->getRoles());
-                $errors = $validator->validate($user);
-                if(count($errors))
-                 {
-                    $errors = $serializer->serialize($errors, 'json');
-                    return new Response($errors, 500, [
-                        'Content-Type' => 'application/json'
-                    ]);
-                }
-                $entityManager=$this->getDoctrine()->getManager(); 
-                $entityManager->persist($user);
-                $entityManager->flush();
-
-                $data = [
-                    'status' => 201,
-                    'message' => 'bien insere'
-                ];
-                return new JsonResponse($data,201);
-    
-                $data = [
-                    'status' => 500,
-                    'message' => 'renseignez les champs'
-                ];
-                return new JsonResponse($data,500);
-            }
-        }
 
 }
